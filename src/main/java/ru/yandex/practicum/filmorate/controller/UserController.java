571b860569c72b5ca5cec95623375202bd6e3d3c
alekseyshiyanov <shiyanov.aleksey.a@gmail.com>
2023-02-01 22:03:25 +0300

    Внесены изменения в класс UserController

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exceptions.FilmorateBadRequestException;
 import ru.yandex.practicum.filmorate.exceptions.FilmorateNotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.UserService;
 
 import javax.validation.Valid;
 import java.util.*;
@@ -18,72 +20,28 @@ import java.util.*;
 @Slf4j
 public class UserController {
 
-    private Long userUID = 0L;
-    private final HashMap<Long, User> users = new HashMap<>();
+    private final UserService userService;
 
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
     @GetMapping
     public List<User> getUsersList() {
-        log.info("Текущее количество пользователей: {}", users.size());
-        return new ArrayList<>(users.values());
+        log.info("Получен запрос на  получение списка пользователей");
+        return userService.getUsersList();
     }
 
     @PostMapping
     public User createUser(@Valid @RequestBody User user) {
-        addUser(user);
-        return user;
+        log.info("Создание новой записи");
+        return userService.createUser(user);
     }
 
     @PutMapping
     public User updateUser(@Valid @RequestBody User user) {
-        Long uid = user.getId();
-
-        if (uid == null) {
-            log.info("Объект: {} не может быть сохранен. Причина 'ID не должен быть null'", user);
-            throw new FilmorateBadRequestException("Ошибка обновления объекта. ID не должен быть null");
-        }
-
-        if (uid < 0L) {
-            log.info("Объект: {} не может быть сохранен. Причина 'ID должен быть положительным числом'", user);
-            throw new FilmorateBadRequestException("Ошибка обновления объекта. ID должен быть положительным числом");
-        }
-
-        User oldUser = users.get(user.getId());
-
-        if (oldUser == null) {
-            log.info("Объект: {} не может быть сохранен. Причина 'Объект с ID = {} не существует'", user, uid);
-            throw new FilmorateNotFoundException("Ошибка обновления объекта. Объект с ID = " + uid + " не существует");
-        }
-
-        log.info("Обновляем объект с ID: {}", oldUser.getId());
-        log.info("Объект до обновления: {}", oldUser);
-
-        userAddOrUpdate(user);
-
-        log.info("Объект после обновления: {}", users.get(user.getId()));
-
-        return user;
-    }
-
-    private void addUser(User user) {
-        Long uid = getUserUID();
-        user.setId(uid);
-        userAddOrUpdate(user);
-        log.info("Сохранен объект: {}", user);
-    }
-
-    private void checkUserName(User user) {
-        if ((user.getName() == null) || user.getName().isEmpty() || user.getName().isBlank()) {
-            user.setName(user.getLogin());
-        }
-    }
-
-    private void userAddOrUpdate(User user) {
-        checkUserName(user);
-        users.put(user.getId(), user);
-    }
-
-    private Long getUserUID() {
-        return ++userUID;
+        log.info("Запрос на обновление текущей записи");
+        return userService.updateUser(user);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
