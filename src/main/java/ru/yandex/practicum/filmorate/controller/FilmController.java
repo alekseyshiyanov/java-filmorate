1b0450955f3464a759c1f79ed91b5f56d89a8a11
alekseyshiyanov <shiyanov.aleksey.a@gmail.com>
2023-02-01 22:01:17 +0300

    Внесены изменения в класс FilmController

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -9,11 +10,10 @@ import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exceptions.FilmorateBadRequestException;
 import ru.yandex.practicum.filmorate.exceptions.FilmorateNotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.service.FilmService;
 
 import javax.validation.Valid;
-import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -21,83 +21,30 @@ import java.util.List;
 @Slf4j
 public class FilmController {
 
-    private Long filmUID = 0L;
-    private final HashMap<Long, Film> films = new HashMap<>();
+    private final FilmService filmService;
 
-    private Long getFilmUID() {
-        return ++filmUID;
+    @Autowired
+    public FilmController(FilmService filmService) {
+        this.filmService = filmService;
     }
 
     @GetMapping
     public List<Film> getFilmsList() {
-        log.info("Текущее количество фильмов: {}", films.size());
-        return new ArrayList<>(films.values());
+        log.info("Запрос на получение списка фильмов");
+        return filmService.getFilmsList();
     }
-
 
     @PostMapping
     public Film createFilm(@Valid @RequestBody Film film) {
-        addFilm(film);
-        return film;
+        log.info("Запрос на создание новой записи");
+        return filmService.createFilm(film);
     }
 
     @PutMapping
     public Film updateFilm(@Valid @RequestBody Film film) {
-        Long uid = film.getId();
-
-        if (uid == null) {
-            log.info("Объект: {} не может быть сохранен. Причина 'ID не должен быть null'", film);
-            throw new FilmorateBadRequestException("Ошибка обновления объекта. ID не должен быть null");
-        }
-
-        if (uid < 0L) {
-            log.info("Объект: {} не может быть сохранен. Причина 'ID должен быть положительным числом'", film);
-            throw new FilmorateBadRequestException("Ошибка обновления объекта. ID должен быть положительным числом");
-        }
-
-        Film oldFilm = films.get(film.getId());
-
-        if (oldFilm == null) {
-            log.info("Объект: {} не может быть сохранен. Причина 'Объект с ID = {} не существует'", film, uid);
-            throw new FilmorateNotFoundException("Ошибка обновления объекта. Объект с ID = " + uid + " не существует");
-        }
-
-        log.info("Обновляем объект с ID: {}", oldFilm.getId());
-        log.info("Объект до обновления: {}", oldFilm);
-
-        filmAddOrUpdate(film);
-
-        log.info("Объект после обновления: {}", films.get(film.getId()));
-
-        return film;
+        log.info("Запрос на обновление текущей записи");
+        return filmService.updateFilm(film);
     }
-
-    private void addFilm(Film film) {
-        film.setId(getFilmUID());
-        filmAddOrUpdate(film);
-        log.info("Сохранен объект: {}", film);
-    }
-
-    private void checkReleaseDate(Film film) {
-        LocalDate releaseDate = film.getReleaseDate();
-
-        if (releaseDate == null) {
-            log.info("Объект: {} не может быть сохранен. Причина 'Дата релиза не может быть null'", film);
-            throw new FilmorateBadRequestException("Дата релиза не может быть null");
-        }
-
-        if(releaseDate.isBefore(LocalDate.of(1895, 12, 28))) {
-            log.info("Объект: {} не может быть сохранен. Причина 'Дата релиза не может быть ранее 1895-12-28'", film);
-            throw new FilmorateBadRequestException("Дата релиза не может быть ранее 1895-12-28");
-        }
-    }
-
-    private void filmAddOrUpdate(Film film) {
-        checkReleaseDate(film);
-        films.put(film.getId(), film);
-    }
-
-
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
