0710174162ab355d09b3c58d1015aea4fd9e5d59
alekseyshiyanov <shiyanov.aleksey.a@gmail.com>
2023-02-04 14:21:57 +0300

    Добавлена работа с лайками. Добавлено получене фильма по id.

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -2,6 +2,8 @@ package ru.yandex.practicum.filmorate.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exceptions.FilmorateBadRequestException;
+import ru.yandex.practicum.filmorate.exceptions.FilmorateNotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 
@@ -27,5 +29,63 @@ public class FilmService {
 
     public Film updateFilm(Film film) {
         return filmStorage.updateFilm(film);
+    }
+
+    public Film getFilm(Long filmId) {
+        checkFilmId(filmId);
+
+        Film film = filmStorage.getFilm(filmId);
+        if (film == null) {
+            throw new FilmorateNotFoundException("Объект с id = " + filmId + " не найден");
+        }
+        return film;
+    }
+
+    public Film addLike(Long filmId, Long userId) {
+        checkFilmId(filmId);
+        checkUserId(userId);
+
+        Film film = filmStorage.addLike(filmId, userId);
+
+        if (film == null) {
+            throw new FilmorateNotFoundException("Запись типа Film с id = " + filmId + " не найдена");
+        }
+
+        return film;
+    }
+
+    public Film deleteLike(Long filmId, Long userId) {
+        checkFilmId(filmId);
+        checkUserId(userId);
+
+        Film film = filmStorage.deleteLike(filmId, userId);
+
+        if (film == null) {
+            throw new FilmorateNotFoundException("Запись типа Film с id = " + filmId + " не найдена");
+        }
+
+        return film;
+    }
+
+    public List<Film> likedFilmsList (Long count) {
+        Long filmsCount = 10L;
+
+        if (count != null) {
+            filmsCount = count;
+        }
+
+        return filmStorage.likedFilmsList(filmsCount);
+    }
+
+    private void checkFilmId(Long filmId) {
+        if (filmId < 0) {
+            throw new FilmorateBadRequestException("Параметр 'id' не может быть отрицательным");
+        }
+    }
+
+    private void checkUserId(Long userId) {
+        if (userId < 0) {
+            throw new FilmorateBadRequestException("Параметр 'userId' не может быть отрицательным");
+        }
     }
 }
