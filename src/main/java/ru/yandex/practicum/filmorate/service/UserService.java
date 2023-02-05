a31a2dbde0f06c4ff0f36b2d4b5d74bf23524047
alekseyshiyanov <shiyanov.aleksey.a@gmail.com>
2023-02-05 03:37:43 +0300

    Внесены изменения для работы со списками друзей

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.filmorate.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exceptions.FilmorateNotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
@@ -25,8 +26,49 @@ public class UserService {
         return userStorage.createUser(user);
     }
 
-
     public User updateUser(User user) {
         return userStorage.updateUser(user);
+    }
+
+    public User getUser(Long userId) {
+        checkUserId(userId);
+
+        User user = userStorage.getUser(userId);
+        if (user == null) {
+            throw new FilmorateNotFoundException("Пользователь с id = " + userId + " не найден");
+        }
+        return user;
+    }
+
+    public void addFriends(Long userId, Long friendId) {
+        checkUserId(friendId);
+        checkUserId(userId);
+
+        userStorage.addFriends(userId, friendId);
+    }
+
+    public void deleteFriends(Long userId, Long friendId) {
+        checkUserId(friendId);
+        checkUserId(userId);
+        userStorage.deleteFriend(userId, friendId);
+    }
+
+    public List<User> getFriendsList(Long userId) {
+        checkUserId(userId);
+
+        return userStorage.getFriendsList(userId);
+    }
+
+    public List<User> getCommonFriendsList(Long userId, Long otherId) {
+        checkUserId(userId);
+        checkUserId(otherId);
+
+        return userStorage.getCommonFriendsList(userId, otherId);
+    }
+
+    private void checkUserId(Long userId) {
+        if (userId < 0) {
+            throw new FilmorateNotFoundException("Пользователь с userId = " + userId + " не существует");
+        }
     }
 }
