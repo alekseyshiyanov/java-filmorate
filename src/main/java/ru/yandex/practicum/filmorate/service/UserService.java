17e3c1aca622ab293f22d7d465e8843f22454648
Aleksey.Shiyanov <shiyanov.aleksey.a@gmail.com>
2023-03-06 16:28:39 +0300

    Внесены изменения в класс UserService

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.exceptions.FilmorateNotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
@@ -11,12 +12,11 @@ import java.util.List;
 @Service
 public class UserService {
 
-    private final UserStorage userStorage;
-
     @Autowired
-    public UserService(UserStorage userStorage) {
-        this.userStorage = userStorage;
-    }
+    @Qualifier("userDbStorage")
+    private UserStorage userStorage;
+
+    public UserService() {}
 
     public List<User> getUsersList() {
         return userStorage.getUsersList();
