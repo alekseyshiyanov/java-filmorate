91130af79233e69d0580dcf60d1c29fe17ef0ff8
Aleksey.Shiyanov <shiyanov.aleksey.a@gmail.com>
2023-03-06 16:27:44 +0300

    Внесены изменения в классы Film и User

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,18 +1,24 @@
 package ru.yandex.practicum.filmorate.model;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
+import lombok.Builder;
 import lombok.Data;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import ru.yandex.practicum.filmorate.model.genre.Genre;
+import ru.yandex.practicum.filmorate.model.mpa.MPA;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Film {
@@ -23,12 +29,36 @@ public class Film {
     @Size(max = 200, message = "Максимальная длина описания — не более 200 символов")
     private String description;
 
+    private List<Genre> genres;
+
+    private MPA mpa;
+
     private LocalDate  releaseDate;
 
     @Positive(message = "Продолжительность фильма должна быть положительной")
     private Integer duration;
 
+    @Positive(message = "Количество лайков должно быть положительным")
+    private Integer likesCount;
+
     private Set<Long> likesList;
+
+    public void setGenres(List<Genre> genres) {
+        this.genres = Objects.isNull(genres) ? new ArrayList<>() : genres;
+    }
+
+    public void setMpa(MPA mpa) {
+        this.mpa = Objects.isNull(mpa) ? new MPA() : mpa;
+    }
+
+    public void setLikesCount(Integer likesCount) {
+        this.likesCount = Objects.isNull(likesCount) ? 0 : likesCount;
+    }
+
+    public Integer getLikesCount() {
+        likesCount = Objects.isNull(likesCount) ? 0 : likesCount;
+        return likesCount;
+    }
 
     @Override
     public boolean equals(Object o) {
@@ -41,6 +71,7 @@ public class Film {
         if (!Objects.equals(description, film.description)) return false;
         if (!Objects.equals(releaseDate, film.releaseDate)) return false;
         if (!Objects.equals(duration, film.duration)) return false;
+        if (!Objects.equals(likesCount, film.likesCount)) return false;
         return Objects.equals(likesList, film.likesList);
     }
 
@@ -50,6 +81,7 @@ public class Film {
         result = 31 * result + (description != null ? description.hashCode() : 0);
         result = 31 * result + (releaseDate != null ? releaseDate.hashCode() : 0);
         result = 31 * result + (duration != null ? duration.hashCode() : 0);
+        result = 31 * result + (likesCount != null ? likesCount.hashCode() : 0);
         result = 31 * result + (likesList != null ? likesList.hashCode() : 0);
         return result;
     }
